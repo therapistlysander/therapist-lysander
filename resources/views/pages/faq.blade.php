7f66f4e0820fdd2d9bd9dd116c5aaaf443796d42
rakeshmaity271 <rakeshmaity271@gmail.com>
2026-06-19 19:54:30 +0530

    Update FAQ page view

--- resources/views/pages/faq.blade.php
+++ resources/views/pages/faq.blade.php
@@ -136,11 +136,11 @@
           <!-- Bottom CTA -->
           <div class="faq-cta fade-in">
             <div>
-              <h3>{{ __('ui.faq.still_questions') }}</h3>
-              <p>{{ __('ui.faq.still_questions_desc') }}</p>
+              <h3>{{ $ctaSec?->content['heading'] ?? __('ui.faq.still_questions') }}</h3>
+              <p>{!! $ctaSec?->content['body'] ?? __('ui.faq.still_questions_desc') !!}</p>
             </div>
-            <a href="{{ route('contact') }}" class="btn btn--primary btn--lg" style="white-space:nowrap;">
-              {{ __('ui.common.contact_me') }}
+            <a href="{{ $ctaSec?->content['cta_url'] ?? route('contact') }}" class="btn btn--primary btn--lg" style="white-space:nowrap;">
+              {{ $ctaSec?->content['cta_label'] ?? __('ui.common.contact_me') }}
               <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" width="18" height="18"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"/></svg>
             </a>
           </div>
