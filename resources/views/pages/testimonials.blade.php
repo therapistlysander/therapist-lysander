a19a1c70a74c54e4a2f768a5c16d8be500f65824
rakeshmaity271 <rakeshmaity271@gmail.com>
2026-07-10 02:24:39 +0530

    fix: use body field for testimonial card text instead of short_description

--- resources/views/pages/testimonials.blade.php
+++ resources/views/pages/testimonials.blade.php
@@ -241,7 +241,7 @@
         @foreach($testimonials as $t)
         <div class="testimonial-card fade-in">
           <div class="testimonial-card__quote-mark">&ldquo;</div>
-          <p class="testimonial-card__text">{{ $t->short_description ?? $t->headline ?? $t->quote ?? Str::limit(strip_tags($t->body), 120) }}</p>
+          <p class="testimonial-card__text">{{ $t->body ? Str::limit(strip_tags($t->body), 120) : ($t->headline ?? $t->short_description ?? '') }}</p>
           <div class="testimonial-card__divider"></div>
           <p class="testimonial-card__name">&mdash; {{ $t->client_name }}</p>
           <p class="testimonial-card__role">Client</p>
